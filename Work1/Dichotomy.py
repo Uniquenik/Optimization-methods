aab42b470aa3047843f86c658b7fbb3d076bb406
Nikita Vanyuchenko <postamentbox@bk.ru>
2022-02-21 18:45:39 +0300

    Dichotomy

--- Work1/Dichotomy.py
+++ Work1/Dichotomy.py
@@ -17,12 +17,17 @@ def dichotomy(a, b, eps, delta, n):
         k += 1
         x_n = (a_n + b_n - delta) / 2.0
         y_n = (a_n + b_n + delta) / 2.0
+        print("Step", k)
+        print("x =", x_n, ",y =", y_n)
         if f(x_n) < f(y_n):
             b_n = y_n
+            print(f(x_n), "<", f(y_n))
         else:
             a_n = x_n
-    print((a_n + b_n) / 2)
-    print("k = " + str(k) + ", N = " + str(2 * (k + 1)))
+            print(f(x_n), ">=", f(y_n))
+        print("L =[", a_n, ";", b_n, "] and", eps)
+    print("\nAnswer:", (a_n + b_n) / 2)
+    print("k =", k, ", N =", 2 * (k + 1))
 
 
 dichotomy(a, b, eps, delta, n)
